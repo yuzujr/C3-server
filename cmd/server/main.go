193f02eb65e4d177a3a4a46d00adb6e230f49d14
yuzujr <15568103056@163.com>
2025-07-24 20:17:10 +0800

    feat: 服务中断后清理客户在线状态

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"strconv"
+	"syscall"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -13,12 +15,12 @@ import (
 	"github.com/yuzujr/C3/internal/logger"
 	"github.com/yuzujr/C3/internal/middleware"
 	"github.com/yuzujr/C3/internal/routes"
+	"github.com/yuzujr/C3/internal/service"
 	"github.com/yuzujr/C3/internal/websocket"
 )
 
 func main() {
 	// 加载环境变量
-	// 这里假设 .env 文件在项目根目录
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
@@ -38,42 +40,51 @@ func main() {
 	r.Use(gin.Recovery())
 
 	// 静态文件
-	// 获取cwd
 	cwd, err := os.Getwd()
 	if err != nil {
 		log.Fatalf("os.Executable: %v", err)
 	}
-	// web目录必须在cwd下
-	// UPLOAD_DIR可以是绝对路径或相对路径
 	r.Static("/static", filepath.Join(cwd, "web"))
 	r.Static("/uploads", config.Get().Upload.Directory)
 
 	// 注册路由
-	// 登录页面路由
 	r.GET("/login", func(c *gin.Context) {
 		c.File(filepath.Join("./web", "login.html"))
 	})
-	// 主页重定向到登录检查
 	r.GET("/", middleware.RequireAuth(), func(c *gin.Context) {
 		c.File(filepath.Join("./web", "index.html"))
 	})
-	// 客户端api
 	clientGroup := r.Group(cfg.Server.BasePath + "client")
 	routes.RegisterClientRoutes(clientGroup)
-	// 登录api
 	authGroup := r.Group(cfg.Server.BasePath + "auth")
 	routes.RegisterAuthRoutes(authGroup)
-	// 前端api
 	webGroup := r.Group(cfg.Server.BasePath + "web")
 	routes.RegisterWebRoutes(webGroup)
-	// WebSocket连接api
 	wsGroup := r.Group(cfg.Server.BasePath + "ws")
 	routes.RegisterWsRoutes(wsGroup)
 
-	// 启动
+	// 设置服务器监听地址
 	addr := cfg.Server.Host + ":" + strconv.Itoa(cfg.Server.Port)
 	logger.Infof("Access the web interface at http://%s", addr)
-	if err := r.Run(addr); err != nil {
-		logger.Errorf("Failed to start server: %v", err)
+
+	// 监听中断信号（ctrl+c）以执行清理操作
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// 启动 HTTP 服务并等待中断信号
+	go func() {
+		if err := r.Run(addr); err != nil {
+			logger.Errorf("Failed to start server: %v", err)
+		}
+	}()
+
+	// 等待收到中断信号，执行清理操作
+	<-sigChan
+	// 执行清理操作，将所有客户端状态设置为下线
+	logger.Infof("Shutting down gracefully...")
+	clients, _ := service.GetAllClients()
+	for _, c := range clients {
+		c.Online = false
+		service.UpdateClient(c)
 	}
 }
